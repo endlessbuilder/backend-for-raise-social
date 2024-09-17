4c7a4946975ed0a79d2ee0495afcd5821992ec3c
endlessbuilder <continuebuilding@gmail.com>
2024-09-17 02:43:04 -0400

    update: vercel

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(fileUpload({
 
 app.use(
   cors({
-    origin: ["https://frontend-for-raise-social.vercel.app", "https://frontend-for-raise-social.vercel.app"], // Update as necessary
+    origin: ["https://frontend-for-raise-social.vercel.app"], // Update as necessary
   }),
 );
 
