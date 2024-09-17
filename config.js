3f105335e4cb5d94a6ed4120e4d7bc15f5a62a97
endlessbuilder <continuebuilding@gmail.com>
2024-09-17 12:40:28 -0400

    update: vercel

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ module.exports = {
   secret: "super secret passphrase",
   db_collection_prefix: "raise_",
   lang: "en",
-  allowed_origin: ["http://localhost:3000", "https://localhost:4000"],
+  allowed_origin: ["https://frontend-for-raise-social.vercel.app"],
 
   expiresIn: 3600 * 72, // 3 days
-  db_url: "mongodb://127.0.0.1:27017/raise",
+  db_url: "mongodb+srv://admin:solana@raise-shard-00-00.3twoe.mongodb.net/raise",
   db_options: {
     // useNewUrlParser: true, //from 6 or higher version of mongoose
     // useUnifiedTopology: true, // the same above
   },
   serviceUrl:
-    process.env.REACT_APP_SERVICE_URL || "http://localhost:5005/api",
+    process.env.REACT_APP_SERVICE_URL || "https://frontend-for-raise-social.vercel.app/api",
 };
