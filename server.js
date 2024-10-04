31d10bd845cf3eeed9310ea5d58c4dd53e58375e
Augusta <emokimet22@gmail.com>
2024-10-04 18:42:09 +0400

    add cors 1

--- server.js
+++ server.js
@@ -31,30 +31,30 @@ app.use(fileUpload({
   tempFileDir: "/tmp/",
 }));
 
-// app.use(
-//   cors({
-//     origin: ["*"], // Update as necessary  "https://frontend-for-raise-social.vercel.app"
-//   }),
-// );
+app.use(
+  cors({
+    origin: ["*"], // Update as necessary  "https://frontend-for-raise-social.vercel.app"
+  }),
+);
 
-const allowedOrigins = [
-  'http://localhost:3000', // First frontend
-  'https://frontend-for-raise-social.vercel.app',    // Second frontend
-];
+// const allowedOrigins = [
+//   'http://localhost:3000', // First frontend
+//   'https://frontend-for-raise-social.vercel.app',    // Second frontend
+// ];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true, // if you need to allow credentials
-}));
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true, // if you need to allow credentials
+// }));
 
 // Connect to MongoDB
 console.log(process.env.MONGO_URI)
