634cd588a744c356e715f918bccade14837fe85c
Augusta <emokimet22@gmail.com>
2024-10-04 18:40:35 +0400

    add cors

--- server.js
+++ server.js
@@ -37,6 +37,25 @@ app.use(fileUpload({
 //   }),
 // );
 
+const allowedOrigins = [
+  'http://localhost:3000', // First frontend
+  'https://frontend-for-raise-social.vercel.app',    // Second frontend
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // if you need to allow credentials
+}));
+
 // Connect to MongoDB
 console.log(process.env.MONGO_URI)
 const db = process.env.MONGO_URI;
